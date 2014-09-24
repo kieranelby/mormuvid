23219e5b5a8dcebbdc918f54e9ddb346214c4cdc
Kieran Elby <kieran@dunelm.org.uk>
2014-09-24 23:15:04 +0100

    Add start-up code

---
+++ mormuvid/app.py
@@ -0,0 +1,37 @@
+import logging
+import time
+
+from pykka.registry import ActorRegistry
+
+from mormuvid.librarian import Librarian
+from mormuvid.downloader import DownloaderActor
+from mormuvid.finder import FinderActor
+from mormuvid.scout import ScoutActor
+
+logger = logging.getLogger(__name__)
+
+class App:
+    """
+    Starts everything up ...
+    """
+
+    def start(self):
+        logger.info("starting up ...")
+        librarian = Librarian()
+        downloader = DownloaderActor.start(librarian).proxy()
+        finder = FinderActor.start(librarian, downloader).proxy()
+        scout = ScoutActor.start(librarian, finder)
+        self._wait_for_interrupt()
+        logger.info("waiting for actors to stop ...")
+        try:
+            ActorRegistry.stop_all(timeout=2)
+        finally:
+            logger.info("exiting")
+
+    def _wait_for_interrupt(self):
+        while True:
+            try:
+                # TODO - must find better way!
+                time.sleep(1)
+            except (KeyboardInterrupt, SystemExit):
+                break
